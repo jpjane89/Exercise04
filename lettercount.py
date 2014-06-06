f34234b39da07f1602213b47100ae6b0ec9e49a8
Hackbright Student <info@hackbrightacademy.com>
2014-06-06 11:01:49 -0700

    lettercount version one working

--- lettercount.py
+++ lettercount.py
@@ -9,14 +9,10 @@ text = my_file.read()
 letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 letter_count = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-for char in my_file:
-    if ord(char.lower()) > 96 and ord(char.lower()) < 123:
-        index = letters.index(char)
+for char in text:
+    if char.lower() in letters:
+        index = letters.index(char.lower())
         letter_count[index] += 1
 
-print letter_count
-
-
-
-
-
+for l in letter_count:        
+    print l
